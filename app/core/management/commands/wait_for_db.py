98c5b67854ae69b1659185a796b4e664e8e77be4
dondonttoxic <donnod837@gmail.com>
2021-06-11 10:03:46 +0700

    Config deploy heroku

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,4 +1,5 @@
 import time
+
 from django.db import connections
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
@@ -6,6 +7,7 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     """Django command to pause execution until database is available"""
+
     def handle(self, *args, **options):
         self.stdout.write('Waiting for database...')
         db_conn = None
@@ -15,4 +17,5 @@ class Command(BaseCommand):
             except OperationalError:
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
+
         self.stdout.write(self.style.SUCCESS('Database available!'))
